71abfed877208ecf5cf6472829e7da301cc05591
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-02-20 03:50:58 +0100

    Publish ready

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
-    isPublic: z.boolean().optional(),
+    isPublic: z.boolean().optional() ?? true,
     title: z.string(),
     description: z.string().transform((value) => value.replace(/\n/g, "<br>")),
     // type: is string but can be empy with ?
